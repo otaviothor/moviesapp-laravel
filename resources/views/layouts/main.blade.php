78183ab9308f5e629e8b42c11f4c3388145945d3
Otávio Barreto de Oliveira Silva <otaviosilva2632@gmail.com>
2020-05-11 23:56:50 -0300

    search dropdown with livewire

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -8,6 +8,7 @@
   <title>MoviesApp</title>
 
   <link rel="stylesheet" href="/css/main.css">
+  <livewire:styles>
 </head>
 
 <body class="font-sans bg-gray-900 text-white">
@@ -34,12 +35,7 @@
         </li>
       </ul>
       <div class="flex flex-col md:flex-row items-center">
-        <div class="relative mt-3 md:mt-0">
-          <input type="text" placeholder="Search" id="" class="bg-gray-800 rounded-full w-64 px-4 pl-8 py-1 focus:outline-none focus:shadow-outline">
-          <div class="absolute top-0">
-            <svg class="fill-current w-4 text-gray-500 mt-2 ml-2" viewBox="0 0 24 24"><path class="heroicon-ui" d="M16.32 14.9l5.39 5.4a1 1 0 01-1.42 1.4l-5.38-5.38a8 8 0 111.41-1.41zM10 16a6 6 0 100-12 6 6 0 000 12z"/></svg>
-          </div>
-        </div>
+        <livewire:search-dropdown>
         <div class="md:ml-4 mt-3 md:mt-0">
           <a href="">
             <img src="https://avatars0.githubusercontent.com/u/48496163?s=460&u=f08f2f46f222646321330cc9a00fb271b08aeaa2&v=4" class="rounded-full w-10 h-10">
@@ -49,6 +45,7 @@
     </div>
   </nav>
   @yield('content')
+  <livewire:scripts>
 </body>
 
 </html>
